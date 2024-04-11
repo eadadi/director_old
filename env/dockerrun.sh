b84a18abad09f5331929969af7ca4ab98e336a48
Eaad <eadadi@gmail.com>
2024-04-11 21:41:36 +0300

    fixed logdir bug

--- env/dockerrun.sh
+++ env/dockerrun.sh
@@ -3,7 +3,7 @@
 # Launch from director main folder
 docker run --rm -it --network host \
 	-v $(pwd):/code \
-	-v /home/ec2-user/drive/project/logdit:/logdir \
+	-v /home/ec2-user/drive/project/logdir:/logdir \
 	-w /code \
 	--gpus all \
 	mydirector bash
