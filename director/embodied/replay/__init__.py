fdd02c426aac760b69a833f51b953db2eb1ba303
Eaad <eadadi@gmail.com>
2024-04-14 15:17:33 +0300

    align embodied/replays with r2i

--- director/embodied/replay/__init__.py
+++ director/embodied/replay/__init__.py
@@ -5,3 +5,11 @@ from .consecutive import Consecutive
 from .fixed_length import FixedLength
 from .prioritized import Prioritized
 from .dispatch import Dispatch
+
+from .generic import Generic
+from .generic_lfs import FIFO_LFS
+from .reverb import Reverb
+from .replays import Uniform
+from .naive_chunks import NaiveChunks
+from . import selectors
+from . import limiters
